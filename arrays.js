da7c66d5d57d41c45593481b4463105a50ed9070
Denis774 <51455035+Denis774@users.noreply.github.com>
2019-06-19 08:32:36 +0300

    Printing Prime Numbers

--- arrays.js
+++ arrays.js
@@ -1,12 +1,15 @@
-function primeNumbersTo(n)
-{
+function primeNumbersTo(Max)
+
+        {
+            
     var store  = [], i, j, primeNumbers = []; // This step calls for initialisation the array to store values
+   
     for (i = 2; i <= 100; ++i) 
     {
         if (!store [i]) 
           {
             primeNumbers.push(i); //populates the array
-            for (j = i << 1; j <= n; j += i) 
+            for (j = i << 1; j <= 100; j += i) 
             {
                 store[j] = true;
             }
