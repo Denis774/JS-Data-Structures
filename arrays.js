f0438cb4b67450ca7468886b38f497590378b536
Denis774 <51455035+Denis774@users.noreply.github.com>
2019-06-19 08:15:43 +0300

    Add files via upload

---
+++ arrays.js
@@ -0,0 +1,18 @@
+function primeNumbersTo(n)
+{
+    var store  = [], i, j, primeNumbers = []; // This step calls for initialisation the array to store values
+    for (i = 2; i <= 100; ++i) 
+    {
+        if (!store [i]) 
+          {
+            primeNumbers.push(i); //populates the array
+            for (j = i << 1; j <= n; j += i) 
+            {
+                store[j] = true;
+            }
+        }
+    }
+    return primeNumbers;
+}
+
+console.log(primeNumbersTo(100));
