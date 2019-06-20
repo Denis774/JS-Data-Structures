8eb74c774b46b024b1c6c84044e8385d8ffd148a
Denis774 <51455035+Denis774@users.noreply.github.com>
2019-06-20 09:15:38 +0300

    Add files via upload

---
+++ Contact .js
@@ -0,0 +1,32 @@
+function AddressBook() 
+{
+  this.contacts = []
+}
+// contacts list
+function Contact(first_name, second_name, phone_number, email, address) /*The constructor function */
+ {
+  // The properties of this object
+  this.first_name = first_name,
+  this.second_name = second_name,
+  this.phone_number = phone_number,
+  this.email = email,
+  this.address = address
+}
+
+Contact.prototype.full_name = function() 
+
+{
+	
+  console.log("Your full_name:"  + this.first_name + " " + this.second_name);
+	}
+var Contact = new Contact ("Denis", "Ahimbisibwe", "+256752202389", "ahimbisibwedenis@yahoo.com",240);
+Contact.full_name(); //Sets new object's prototype property to be the constructor function's prototype object and returns the newly created object.
+
+
+
+
+
+
+
+
+
